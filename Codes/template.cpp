1a08b5e84c1cba2a55d17a940b7b94db227e92e2
MarcosJLR <socram.693@gmail.com>
2019-07-30 07:40:46 -0400

    Added template

---
+++ Codes/template.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+#define FOR(i,n,m) for(int i=n; i<m; i++)
+#define ROF(i,n,m) for(int i=n; i>m; i--)
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+#define ri(a) scanf("%d",&a)
+#define rii(a,b) ri(a),ri(b);
+#define riii(a,b,c) rii(a,b),ri(c);
+#define lri(a) scanf("%lld",&a)
+#define lrii(a,b) lri(a),lri(b);
+#define ms(obj,val) memset(obj,val,sizeof(obj))
+#define all(x) x.begin(),x.end()
+#define debug true
+#define dprintf debug && printf
+
+const int INF = 0x3f3f3f3f;
+const ll INFLL = 1e18;
+const int MOD = 1e9+7;
+const int MAXN = 2e5+5;
+
+
+int main(){
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+
+    return 0;
+}
