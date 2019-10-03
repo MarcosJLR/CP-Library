055541db8760bc6037af8e4ad9386b3a3274445c
MarcosJLR <socram.693@gmail.com>
2019-10-03 12:05:00 -0400

    ST Persistent Tested!

--- Codes/Data-Structures/STPersistent.cpp
+++ Codes/Data-Structures/STPersistent.cpp
@@ -1,14 +1,14 @@
 struct STN{
     STN *l, *r;
-    int sum;
-    STN(int val) : l(0), r(0), sum(val) {}
-    STN(STN *l, STN *r) : l(l), r(r), sum(0){
-        if(l) sum += l->sum;
-        if(r) sum += r->sum;
+    int val;
+    STN(int val) : l(0), r(0), val(val) {}
+    STN(STN *l, STN *r) : l(l), r(r), val(0){
+        if(l) val += l->val;
+        if(r) val += r->val;
     }
 };
 
-int N, int A[MAXN];
+int N, A[MAXN];
 
 STN* STB(int l = 0, int r = N){
     if(r - l < 2) return new STN(A[l]);
@@ -16,8 +16,8 @@ STN* STB(int l = 0, int r = N){
     return new STN(STB(l, m), STB(m, r));
 }
 int STQ(int x, int y, STN* v, int l = 0, int r = N){
-    if(l > r) return 0;
-    if(x == l && y == r) return v->sum;
+    if(l >= r || x >= y) return 0;
+    if(x == l && y == r) return v->val;
     int m = (l+r)>>1;
     return STQ(x, min(y, m), v->l, l, m)
          + STQ(max(x, m), y, v->r, m, r);
@@ -25,7 +25,7 @@ int STQ(int x, int y, STN* v, int l = 0, int r = N){
 STN* STU(int p, int x, STN *v, int l = 0, int r = N){
     if(r - l < 2) return new STN(x);
     int m = (l+r)>>1;
-    if(p <= m)
+    if(p < m)
         return new STN(STU(p, x, v->l, l, m), v->r);
     else
         return new STN(v->l, STU(p, x, v->r, m, r));
